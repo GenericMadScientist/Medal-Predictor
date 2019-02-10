5af689ffbb459e635bc37b29a5ee1f11c8476b1a
GenericMadScientist <rayw1710@hotmail.co.uk>
2019-02-10 17:24:59 +0000

    Make medals column editable.

--- src/main/java/com/gms/tfmedals/gui/AppController.java
+++ src/main/java/com/gms/tfmedals/gui/AppController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.StringConverter;
 
 public class AppController {
     private final ObservableList<MedalResultModel> medals =
@@ -28,11 +30,39 @@ public class AppController {
 
     @FXML
     private void initialize() {
+        medalColumn.setCellFactory(TextFieldTableCell.forTableColumn(new MedalStringConverter()));
+        medalColumn.setOnEditCommit(
+            (TableColumn.CellEditEvent<MedalResultModel, Integer> t) ->
+                (t.getTableView().getItems().get(
+                    t.getTablePosition().getRow())
+                ).setMedals(t.getNewValue())
+            );
+
         duelistColumn.setReorderable(false);
         medalColumn.setReorderable(false);
 
         duelistColumn.setCellValueFactory(new PropertyValueFactory<>("duelistName"));
         medalColumn.setCellValueFactory(new PropertyValueFactory<>("medals"));
         medalTable.setItems(medals);
+    }
+
+    private class MedalStringConverter extends StringConverter<Integer> {
+        @Override
+        public String toString(Integer integer) {
+            if (integer == null) {
+                return null;
+            } else {
+                return integer.toString();
+            }
+        }
+
+        @Override
+        public Integer fromString(String s) {
+            try {
+                return Integer.valueOf(s);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
     }
 }
