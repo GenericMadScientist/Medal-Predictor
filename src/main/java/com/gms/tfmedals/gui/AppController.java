c379feb240a1a3908578115f88fed0d0f9eb1460
GenericMadScientist <rayw1710@hotmail.co.uk>
2019-02-10 15:20:05 +0000

    Add AppController.java.

---
+++ src/main/java/com/gms/tfmedals/gui/AppController.java
@@ -0,0 +1,12 @@
+package com.gms.tfmedals.gui;
+
+import javafx.fxml.FXML;
+
+public class AppController {
+    public AppController() {
+    }
+
+    @FXML
+    private void initialize() {
+    }
+}
