6e2d7d80f8646cdf3ad31bba0e9832d8b0cc6ef9
GenericMadScientist <rayw1710@hotmail.co.uk>
2019-02-10 19:24:56 +0000

    Make Duelist class check constructor args.

--- src/main/java/com/gms/tfmedals/logic/Duelist.java
+++ src/main/java/com/gms/tfmedals/logic/Duelist.java
@@ -5,6 +5,14 @@ final class Duelist {
     private final int id;
 
     Duelist(String name, int id) {
+        if (name == null) {
+            throw new IllegalArgumentException("Name should be a not null string");
+        }
+
+        if (id < 0) {
+            throw new IllegalArgumentException("Duelist ID must be non-negative");
+        }
+
         this.name = name;
         this.id = id;
     }
--- src/test/java/com/gms/tfmedals/logic/DuelistTest.java
+++ src/test/java/com/gms/tfmedals/logic/DuelistTest.java
@@ -3,8 +3,19 @@ package com.gms.tfmedals.logic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DuelistTest {
+    @Test
+    void constructor_WithNullName_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Duelist(null, 0));
+    }
+
+    @Test
+    void constructor_WithNegativeId_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new Duelist("Chumley", -1));
+    }
+
     @Test
     void getDuelistName_WithChumleyName_ShouldReturnChumley() {
         Duelist chumley = new Duelist("Chumley", 5);
