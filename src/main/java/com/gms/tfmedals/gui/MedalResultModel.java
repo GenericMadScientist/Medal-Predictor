181ce06846d735dd722b9827b8c76bd48c53b626
GenericMadScientist <rayw1710@hotmail.co.uk>
2019-02-10 18:58:44 +0000

    Clean up MedalResultModel a little.

--- src/main/java/com/gms/tfmedals/gui/MedalResultModel.java
+++ src/main/java/com/gms/tfmedals/gui/MedalResultModel.java
@@ -1,30 +1,23 @@
 package com.gms.tfmedals.gui;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
-public class MedalResultModel {
-    private final StringProperty duelistName;
+public final class MedalResultModel {
+    private final String duelistName;
     private Integer medals;
 
     MedalResultModel(String duelistName, Integer medals) {
-        this.duelistName = new SimpleStringProperty(duelistName);
+        this.duelistName = duelistName;
         this.medals = medals;
     }
 
     public String getDuelistName() {
-        return duelistName.get();
-    }
-
-    public void setDuelistName(String name) {
-        duelistName.set(name);
+        return duelistName;
     }
 
     public Integer getMedals() {
         return medals;
     }
 
-    public void setMedals(Integer medals) {
+    void setMedals(Integer medals) {
         this.medals = medals;
     }
 }
